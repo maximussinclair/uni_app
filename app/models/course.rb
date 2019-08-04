31cf03fdcc0c47dc935d8a001836a8210f6a1640
maximussinclair <mrmaxsinclair@gmail.com>
2019-08-04 11:32:08 +0100

    Added partials for nav and footer and student model and table

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ApplicationRecord
-
+  validates :id, presence: true
+  validates :short_name, presence: true, length: {minimum: 3, maximum: 8 }
+  validates :name, presence: true, length: {minimum: 5, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 8, maximum: 300 }
 end
