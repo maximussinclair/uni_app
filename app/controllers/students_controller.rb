9c1582d9650cad37b88219e1c8945617eaa34275
maximussinclair <mrmaxsinclair@gmail.com>
2019-08-04 11:50:06 +0100

    Added students listing page and setup initial bits for creating new student

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def new
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
-
+  resources :students, except: [:destroy]
 
 end
