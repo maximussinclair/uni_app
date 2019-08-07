f90d403ad2db45f0ce777e2485bd46a0f3bf79f1
maximussinclair <mrmaxsinclair@gmail.com>
2019-08-07 09:34:02 +0100

    Dried up code

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,49 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update]
 
   def index
     @students = Student.all
   end
 
-  def new
+  def show
+  end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+       flash[:notice] = "You're Signed Up!"
+       redirect_to @student
+    else
+      render 'new'
+    end
+  end
+
+
+  def edit
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "Your update was saved"
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :email)
   end
 
 end
